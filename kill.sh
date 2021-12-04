e9f7162defeffb8fa790582bb98a98d80aaba170
sysadmin <sysadmin@omniamorsaequat.com>
2021-12-04 19:55:14 +0000

    update scripts

--- kill.sh
+++ kill.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # we assume --port 41371 is given as first argument
-sudo_pid=$(ps aux | grep 'sudo node main.js' | awk '{if ($11 == "sudo" && $15 == "41371"){print $2}}')
+sudo_pid=$(ps aux | grep 'sudo node jam_mqtt_main.js' | awk '{if ($11 == "sudo"){print $2}}')
 sudo kill -9 "$sudo_pid"
 
-main_pid=$(ps aux | grep 'node main.js' | awk '{if ($11 == "node" && $14 == "41371"){print $2}}')
+main_pid=$(ps aux | grep 'node jam_mqtt_main.js' | awk '{if ($11 == "node"){print $2}}')
 sudo kill -9 "$main_pid"
