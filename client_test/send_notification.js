0d37dcd2e0d119eca95e5753a9d093a6dfdea74b
dbc201 <denizbcakiroglu@gmail.com>
2022-01-18 12:15:49 -0500

    add username to send_notification.js

--- client_test/send_notification.js
+++ client_test/send_notification.js
@@ -1,10 +1,17 @@
 const path = require("path");
 const fs = require('fs');
-firebase_admin = require("firebase-admin");
+const firebase_admin = require("firebase-admin");
 require("dotenv").config({ path: path.join(__dirname, ".env.local") });
 
-// enter phone's firebase token
-const token = process.env.firebase_phone_token;
+const username = process.env.account_username;
+
+const Database = require("@dab-co/jam-sqlite").Database;
+const database = new Database(process.env.db_path);
+
+const AccountUtils = require("@dab-co/jam-sqlite").Utils.AccountUtils;
+const accountUtils = new AccountUtils(database);
+
+const firebase_token = accountUtils.getNotificationToken(accountUtils.getIdByUsername(username));
 
 const service_account_key = JSON.parse(fs.readFileSync(path.join(__dirname, process.env.firebase_account_key_path), "utf8"));
 firebase_admin.initializeApp({
@@ -22,7 +29,7 @@ const options = {
     timeToLive: 60 * 60 * 24,
 };
 
-firebase_admin.messaging().sendToDevice(token, message, options)
+firebase_admin.messaging().sendToDevice(firebase_token, message, options)
     .then(response => {
         console.log(response);
     })
