f2bdef4e1d065e8727bad1a67605d3596d03bc95
dbc201 <denizbcakiroglu@gmail.com>
2022-02-06 17:33:04 -0500

    add more output to demo_server.js

--- client_test/demo_server.js
+++ client_test/demo_server.js
@@ -7,3 +7,16 @@ const server = require('net').createServer(aedes.handle);
 server.listen(process.env.port, function () {
     console.log('Server started and listening on port ', process.env.port);
 });
+
+aedes.on("publish", function(packet, client) {
+    console.log("------publish------");
+    console.log("---packet---");
+    console.log(packet);
+    if (packet && packet.payload) {
+        console.log("payload:", packet.payload.toString());
+    }
+    console.log("---client---");
+    if (client && client.id) {
+        console.log(client.id);
+    }
+});
